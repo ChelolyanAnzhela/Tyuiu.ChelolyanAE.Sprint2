8297784feff180ae997d2e2ede6a94b74b2e7801
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-10-16 21:32:16 +0500

    Task2

---
+++ Tyuiu.ChelolyanAE.Sprint2.Task2.V19/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ChelolyanAE.Sprint2.Task2.V19.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint2.Task2.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:Оператор if - полная и краткая форма записи                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры и   *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами XY в заштрихованной области *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите значение X:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в закрашенной области");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
