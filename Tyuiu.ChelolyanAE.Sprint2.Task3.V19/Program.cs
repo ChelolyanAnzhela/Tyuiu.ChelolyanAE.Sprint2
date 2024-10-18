3ef6ff22bad456bf923452705f95938ad9693bb3
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-10-18 17:04:44 +0500

    Task3

---
+++ Tyuiu.ChelolyanAE.Sprint2.Task3.V19/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ChelolyanAE.Sprint2.Task3.V19.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint2.Task3.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:Вложенные операторы if - else                                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* С использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной Х с клавиатуры.                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введиьте значение Х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Значение функции {res}");
+            Console.ReadKey();
+
+        }
+    }
+}
