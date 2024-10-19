ca45942dab9581d9fad5a91bbff1a24940f8ad0a
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-10-19 12:57:58 +0500

    Task6

---
+++ Tyuiu.ChelolyanAE.Sprint2.Task6.V9.Lib/DataService.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ChelolyanAE.Sprint2.Task6.V9.Lib
+{
+    public class DataService : ISprint2Task6V9
+    {
+        public string FindDateOfNextDay(int m, int n)
+        {
+            string res = "";
+            int count = m switch
+            {
+                1 => count = 31,
+                2 => count = 28,
+                3 => count = 31,
+                4 => count = 30,
+                5 => count = 31,
+                6 => count = 30,
+                7 => count = 31,
+                8 => count = 31,
+                9 => count = 30,
+                10 => count = 31,
+                11 => count = 30,
+                12 => count = 31,
+                < 1 => throw new ArgumentException($"Номер месяца указан некорректно. Значение {m} не соответствует условию"),
+                > 12 => throw new ArgumentException($"Номер месяца указан некорректно. Значение {m} не соответствует условию"),
+            };
+            if (n < count)
+            {
+                if (n + 1 <= 9)
+                {
+                    res = ($"0{n + 1}.0{m}");
+                }
+                else
+                {
+                    res = ($"{n+1}.{m}");
+                }
+
+;
+            }
+            else
+            {
+                res = ($"{01}.{m + 1}");
+            }
+            return res;
+        }
+
+        
+    }
+}
